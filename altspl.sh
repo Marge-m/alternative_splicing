7efbf8a6d35745cafcfca7c65d4ef47f99898384
Marina Orlova <marina.marge.orlova@gmail.com>
2014-08-18 02:17:24 +0400

    Добавить bash скрипт

---
+++ altspl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+samtools view -bS $1 | samtools sort - 1-sorted	#  first sam
+samtools view -bS $2 | samtools sort - 2-sorted	#  second sam
+cuffdiff $3 1-sorted.bam 2-sorted.bam			#  gtf
+/usr/bin/python cuff.py
+/usr/bin/python cuff2.py
+/usr/bin/python cuff3.py
+sed 's/ /\t/g' file2writeminus.txt > minus.txt
+sed 's/ /\t/g' filetowriteplus.txt > plus.txt
+bedtools intersect -a minus.txt -b plus.txt -wo -v > minus.bed
+bedtools intersect -a plus.txt -b minus.txt -wo -v > plus.bed
+bedtools intersect -a minus.bed -b $4 -wo -f 1 > minusinpeaks.bed		#  peaks-introns
+bedtools intersect -a plus.bed -b $4 -wo -f 1 > plusinpeaks.bed		#  peaks-introns
+sort minusinpeaks.bed | uniq > minus_peaks.txt
+sort plusinpeaks.bed | uniq > plus_peaks.txt
+/usr/bin/python add_changes.py

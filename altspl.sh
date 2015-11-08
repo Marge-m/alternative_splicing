6a3633c60f2941f5330236d41bf3fbab85efaa29
Marge-m <marina-orlova@inbox.ru>
2015-11-08 23:16:42 +0300

    Update altspl.sh

--- altspl.sh
+++ altspl.sh
@@ -4,9 +4,9 @@ samtools view -bS $2 | samtools sort - 2-sorted	#  second sam
 cuffdiff $3 1-sorted.bam 2-sorted.bam -p 4		#  gtf
 python filter_isoforms.py
 python create_coords.py
-python cuff3.py
-sed 's/ /\t/g' file2writeminus.txt > minus.txt
-sed 's/ /\t/g' filetowriteplus.txt > plus.txt
+python sort_changes.py
+sed 's/ /\t/g' minus_changes.txt > minus.txt
+sed 's/ /\t/g' plus_changes.txt > plus.txt
 bedtools intersect -a minus.txt -b plus.txt -wo -v > minus.bed
 bedtools intersect -a plus.txt -b minus.txt -wo -v > plus.bed
 bedtools intersect -a minus.bed -b $4 -wo -f 1 > minusinpeaks.bed		#  peaks-introns

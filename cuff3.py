70f4f91a600b47e4fba4ac676f40108e3789757b
Marge-m <marina-orlova@inbox.ru>
2015-11-08 23:15:53 +0300

    refactoring

--- cuff3.py
+++ cuff3.py
@@ -1,21 +1,21 @@
 def forbed(filewithall, filewithminus, filewithplus):
-    f = open(filewithall)
-    k = open(filewithminus, 'w')
-    n = open(filewithplus, 'w')
-    l = []
-    for line in f:
-        l.append(line.strip().split(' '))
-    for item in l:
-        if float(item[5]) < 0:
-            for i in range(6, len(item)-1, 2):
-                k.write(item[0] + ' ' + item[i] + ' ' + item[i+1] + ' ' + item[1] + '\n')
+    all_changes = open(filewithall)
+    minus = open(filewithminus, 'w')
+    plus = open(filewithplus, 'w')
+    changes = []
+    for line in all_changes:
+        changes.append(line.strip().split(' '))
+    for change in changes:
+        if float(change[5]) < 0:
+            for i in range(6, len(change)-1, 2):
+                minus.write(change[0] + ' ' + change[i] + ' ' + change[i+1] + ' ' + change[1] + '\n')
         else:
-            for i in range(6, len(item)-1, 2):
-                n.write(item[0] + ' ' + item[i] + ' ' + item[i+1] + ' ' + item[1] + '\n')
-    k.close()
-    n.close()
-    f.close()
+            for i in range(6, len(change)-1, 2):
+                plus.write(change[0] + ' ' + change[i] + ' ' + change[i+1] + ' ' + change[1] + '\n')
+    minus.close()
+    plus.close()
+    all_changes.close()
 
-forbed('filewithborden.txt', 'file2writeminus.txt', 'filetowriteplus.txt')
+forbed('filewithborden.txt', 'minus_changes.txt', 'plus_changes.txt')
 
 

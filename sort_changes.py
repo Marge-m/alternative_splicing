5fbf74080207729e2072dfaeb88a55cb492c5981
Marge-m <marina-orlova@inbox.ru>
2015-11-09 00:12:45 +0300

    Update sort_changes.py

--- sort_changes.py
+++ sort_changes.py
@@ -7,7 +7,7 @@ import pandas as pd
 def forbed(filewithall, filewithminus, filewithplus):
     all_changes = open(filewithall)
     changes = []
-    minus_changes = pd.DataFrame(columns=['chr', 'start', 'end', 'gene_id']) # create file for negative changes
+    minus_changes = pd.DataFrame(columns=['chr', 'start', 'end', 'gene_id'])  # create file for negative changes
     plus_changes = pd.DataFrame(columns=['chr', 'start', 'end', 'gene_id']) 
     minus_start = 0
     plus_start = 0
@@ -15,7 +15,7 @@ def forbed(filewithall, filewithminus, filewithplus):
         changes.append(line.strip().split(' '))
     all_changes.close()
     for change in changes:
-        if float(change[5]) < 0: # if change of ISO FPKM to GENE FPKM ratio is negative write to "minus" file
+        if float(change[5]) < 0:  # if change of ISO FPKM to GENE FPKM ratio is negative write to "minus" file
             for i in range(6, len(change)-1, 2):
                 minus_changes.loc[minus_start] = [change[0], change[i], change[i+1], change[1]]
                 minus_start += 1

2fedef90361f0a4755abd3768507b8b15557c576
Marge-m <marina-orlova@inbox.ru>
2015-11-08 23:25:01 +0300

    refactoring

--- sort_changes.py
+++ sort_changes.py
@@ -1,20 +1,25 @@
+import pandas as pd
+
+
 def forbed(filewithall, filewithminus, filewithplus):
     all_changes = open(filewithall)
-    minus = open(filewithminus, 'w')
-    plus = open(filewithplus, 'w')
     changes = []
+    minus_changes = pd.DataFrame(columns=['chr', 'start', 'end', 'gene_id'])
+    plus_changes = pd.DataFrame(columns=['chr', 'start', 'end', 'gene_id'])
+    minus_start = 0
+    plus_start = 0
     for line in all_changes:
         changes.append(line.strip().split(' '))
+    all_changes.close()
     for change in changes:
         if float(change[5]) < 0:
             for i in range(6, len(change)-1, 2):
-                minus.write(change[0] + ' ' + change[i] + ' ' + change[i+1] + ' ' + change[1] + '\n')
+                minus_changes.loc[loc_count] = [change[0], change[i], change[i+1], change[1]]
         else:
             for i in range(6, len(change)-1, 2):
-                plus.write(change[0] + ' ' + change[i] + ' ' + change[i+1] + ' ' + change[1] + '\n')
-    minus.close()
-    plus.close()
-    all_changes.close()
+                plus_changes.loc[loc_count] = [change[0], change[i], change[i+1], change[1]]
+    minus_changes.to_csv(filewithminus, sep=' ')
+    plus_changes.to_csv(filewithplus, sep=' ')
 
 forbed('filewithborden.txt', 'minus_changes.txt', 'plus_changes.txt')
 

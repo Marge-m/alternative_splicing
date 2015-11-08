e443031ed3085a1187a95df94d2db81840d0123e
Marge-m <marina-orlova@inbox.ru>
2015-11-08 23:42:08 +0300

    refactoring

--- altspl.sh
+++ altspl.sh
@@ -5,12 +5,12 @@ cuffdiff $3 1-sorted.bam 2-sorted.bam -p 4		#  gtf
 python filter_isoforms.py
 python create_coords.py
 python sort_changes.py
-sed 's/ /\t/g' minus_changes.txt > minus.txt
-sed 's/ /\t/g' plus_changes.txt > plus.txt
+sed 's/ /\t/g' minus_changes.txt | cut -f2,3,4,5 | sed '1d' > minus.txt
+sed 's/ /\t/g' plus_changes.txt | cut -f2,3,4,5 | sed '1d' > plus.txt
 bedtools intersect -a minus.txt -b plus.txt -wo -v > minus.bed
 bedtools intersect -a plus.txt -b minus.txt -wo -v > plus.bed
 bedtools intersect -a minus.bed -b $4 -wo -f 1 > minusinpeaks.bed		#  peaks-introns
 bedtools intersect -a plus.bed -b $4 -wo -f 1 > plusinpeaks.bed		#  peaks-introns
 sort minusinpeaks.bed | uniq > minus_peaks.txt
 sort plusinpeaks.bed | uniq > plus_peaks.txt
-/usr/bin/python add_changes.py
+python add_changes.py
